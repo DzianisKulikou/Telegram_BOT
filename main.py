9f702552788e07228e7d21e6e3c34006bfb5916b
Dzianis Kulikou <d-rival@mail.ru>
2023-07-01 01:15:02 +0200

    Эхо бот на aiogram

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from bot_token import bot_token
 from aiogram import Bot, Dispatcher
-from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.filters import Command  # чтобы фильтровать апдейты по наличию в них команд (начинающихся со знака "/")
+from aiogram.types import Message  # Апдейты этого типа мы будем ловить эхо-ботом
 
 # Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
 API_TOKEN: str = bot_token
@@ -10,6 +10,7 @@ API_TOKEN: str = bot_token
 bot: Bot = Bot(token=API_TOKEN)
 dp: Dispatcher = Dispatcher()
 print('Я ЖИВ!')
+
 
 # Этот хэндлер будет срабатывать на команду "/start"
 @dp.message(Command(commands=["start"]))
@@ -31,5 +32,8 @@ async def send_echo(message: Message):
     await message.reply(text=message.text)
 
 
+# Инструкция выполнится, если файл с программой будет исполняемым, то есть точкой входа в бот. А если этот файл будет
+# использован в качестве модуля для какого-то другого проекта, то инструкция выполняться не будет.
 if __name__ == '__main__':
-    dp.run_polling(bot)
+    dp.run_polling(bot)  # Запускает поллинг, то есть постоянный опрос сервера Telegram на наличие апдейтов для бота.
+                         # В качестве аргумента в метод диспетчера run_polling нужно передать объект бота.
