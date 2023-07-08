316ee52e78ba0c3329c0c48f676c02703f6fd233
Dzianis Kulikou <d-rival@mail.ru>
2023-07-08 23:22:31 +0200

    Импортируем BaseFilter из aiogram.filters. Создаём класс IsAdmin - фильтр на админа. Создаём хэндлер - сработка на админа.

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ import random
 
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
-from aiogram.filters import Text, Command
+from aiogram.filters import Text, Command, BaseFilter
 from bot_token import bot_token
+
+
+# Собственный фильтр, проверяющий юзера на админа
+class IsAdmin(BaseFilter):
+    def __init__(self, admin_ids: list[int]) -> None:
+        # В качестве параметра фильтр принимает список с целыми числами
+        self.admin_ids = admin_ids
+
+    async def __call__(self, message: Message) -> bool:
+        return message.from_user.id in self.admin_ids
 
 
 # Создаем объекты бота и диспетчера
@@ -18,6 +28,12 @@ ATTEMPTS: int = 6
 
 # Словарь, в котором будут храниться данные пользователя
 users: dict = {}
+
+
+# Этот хэндлер будет срабатывать, если апдейт от админа
+@dp.message(IsAdmin(admin_ids))
+async def answer_if_admins_update(message: Message):
+    await message.answer(text='Вы админ')
 
 
 # Функция возвращающая случайное целое число от 1 до 100
